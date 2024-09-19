1bd180f63437b5dba56cecb05e9f19b4f9b20657
ju4700 <jalal.dev.design@gmail.com>
2024-09-19 23:01:11 +0600

    New File Guests

---
+++ guests.py
@@ -0,0 +1,4 @@
+invitation = ['Alice', 'Bob', 'Charlie']
+print(f"PLease come to the party, {invitation[0].title()}.")
+print(f"PLease come to the party, {invitation[1].title()}.")
+print(f"PLease come to the party, {invitation[2].title()}.")
--- test.py
+++ test.py
@@ -9,3 +9,13 @@ bycycles[0] = 'honda'
 print(bycycles) # ['honda', 'cannondale', 'redline', 'specialized']
 
 bycycles.append('ducati')
+bycycles.insert(0, 'bmw')
+print(bycycles) # ['bmw', 'honda', 'cannondale', 'redline', 'specialized', 'ducati']
+
+del bycycles[1]
+print(bycycles) # ['bmw', 'cannondale', 'redline', 'specialized', 'ducati']
+
+bycycles.insert(2, 'honda')
+print(bycycles) # ['bmw', 'cannondale', 'honda', 'redline', 'specialized', 'ducati']
+last_owned = bycycles.pop()
+print(f"The last bycycle I owned was a {last_owned.title()}.") # The last bycycle I owned was a Ducati.
