b4ac6e59726dc8773620ecd90e3db2bd364f852f
ju4700 <jalal.dev.design@gmail.com>
2024-09-19 22:45:22 +0600

    Learned how to commit now!!

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-mylist = ['a', 'b', 'c', 'd']
-print(mylist[2])
+bycycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bycycles) # ['trek', 'cannondale', 'redline', 'specialized']
+print(bycycles[0]) # trek
+
+massage = f"My first Cycle was {bycycles[0].title()}."
+print(massage) # My first Cycle was Trek.
