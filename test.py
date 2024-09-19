86c660ee9d5af22fd269025e8c3cd5de30248be2
ju4700 <jalal.dev.design@gmail.com>
2024-09-19 22:50:12 +0600

    New On

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ print(bycycles[0]) # trek
 
 massage = f"My first Cycle was {bycycles[0].title()}."
 print(massage) # My first Cycle was Trek.
+
+bycycles[0] = 'honda'
+print(bycycles) # ['honda', 'cannondale', 'redline', 'specialized']
+
+bycycles.append('ducati')
