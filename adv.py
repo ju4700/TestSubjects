8f9d7d7d2e3ac14dc1afce5d356d73b5148e572b
ju4700 <jalal.dev.design@gmail.com>
2024-09-21 20:43:31 +0600

    Refactor code to print greetings and pet suggestions

---
+++ adv.py
@@ -0,0 +1,9 @@
+magic = ['alice', 'bob', 'carol']
+for i in magic:
+    print(f"Hello, {i.title()}!")
+
+animals = ['tiger', 'lion', 'bear']
+for i  in animals:
+    print(f"A {i.title()} would make a great pet.")
+
+print("Any of these animals would make a great pet!")
