c32d1e6b00109504a22e9251d84904990950441a
ju4700 <jalal.dev.design@gmail.com>
2024-09-22 19:22:26 +0600

    Refactor code to handle pizza toppings availability

---
+++ condi.py
@@ -0,0 +1,11 @@
+avaiable_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in avaiable_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!")
+
+#exMPLE_TUPLE = ('a', 'b', 'c', 'd', 'e') / (200, 59)
