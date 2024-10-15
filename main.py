a3cc8c5d0e526c3e62e947b070457876facfa18f
ju4700 <jalal.dev.design@gmail.com>
2024-10-16 00:47:06 +0600

    Refactor main.py to improve input handling and print all lists

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 lis1 = list(map(int, input().split()))
-print(lis1)
-
 lis2 = [int(input()) for i in range(3)]
-print(lis2)
-
 lis3 = []
-i = 1
 for i in range(3):
-    x = int(input())
-    lis3.append(x)
-
-print(lis3)
+	lis3.append(int(input()))
+	
+print(f"lis1: {lis1}, lis2: {lis2}, lis3: {lis3}")
