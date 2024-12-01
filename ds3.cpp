18fa89b941bb4432e93b96437d64d3e27e21248a
ju4700 <jalal.dev.design@gmail.com>
2024-12-01 14:44:34 +0600

    Refactor find function in ds3.cpp to improve efficiency and readability for finding the two largest elements in an array

--- ds3.cpp
+++ ds3.cpp
@@ -1,21 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void find(int a[], int n, int x, int y){
-    int maxi = a[0], k = 0;
-    for(int i = 1; i < n; i++){
-        if(a[i] > maxi){
-            maxi = a[i];
-            x = i;
-        }
-    }int secm = a[0];
-    for (int i = 0; i < n; i++){
-        if(a[i] < maxi && a[i] > secm){
-            secm = a[i];
-            y = i;
+// void find(int a[], int n, int x, int y){
+//     int maxi = a[0], k = 0;
+//     for(int i = 1; i < n; i++){
+//         if(a[i] > maxi){
+//             maxi = a[i];
+//             x = i;
+//         }
+//     } int secm = a[0];
+//     for (int i = 0; i < n; i++){
+//         if(a[i] < maxi && a[i] > secm){
+//             secm = a[i];
+//             y = i;
+//         }
+//     }
+//     cout << "Largest element "<< maxi << "is at " << x << " and  second " << secm << "is at " << y << endl;
+// }
+
+void find(int a[], int n, int l1, int l2){
+    int f = a[0], s = a[1];
+    l1 = 0;
+    l2 = 1;
+    if(f < s){
+        swap(f, s);
+        swap(l1, l2);
+    }
+    for(int i = 2; i < n; i++){
+        if(a[i] > f){
+            s = f;
+            l2 = l1;
+            f = a[i];
+            l1 = i;
+        }else if(a[i] > s){
+            s = a[i];
+            l2 = i;
         }
     }
-    cout << "Largest element "<< maxi << "is at " << x << " and  second " << secm << "is at " << y << endl;
+    cout << "Largest element " << f << " is at " << l1 << " and second " << s << " is at " << l2 << endl;
 }
 
 int main(){
